79d1af190b64270915ceb3b47c4f091f2e2bbf42
Andre S <gymclothes@gmail.com>
2013-12-09 13:19:15 -0600

    fixed from Request data set

--- Lib/Exception/ApiException.php
+++ Lib/Exception/ApiException.php
@@ -9,12 +9,14 @@ class ApiException extends CakeException
     public function __construct($message = 'There was error in the API. Please try again later.', $code = 500)
     {
         if (Configure::read('ApiManager.softErrors')) {
-            
             $fromApi = (
                 (isset($this->getTrace()[0]['function']) && stristr($this->getTrace()[0]['function'], 'api')) && 
                 (isset($this->getTrace()[1]['function']) && stristr($this->getTrace()[1]['function'], 'api'))
             );
-            $fromRequest = isset($this->getTrace()[0]['class']) && stristr($this->getTrace()[0]['class'], 'request');
+            $fromRequest = (
+                (isset($this->getTrace()[0]['class']) && stristr($this->getTrace()[0]['class'], 'request')) &&
+                (isset($this->getTrace()[2]['function']) && stristr($this->getTrace()[2]['function'], 'api'))
+            );
             
             if ($fromApi || $fromRequest) {
                 echo json_encode([
