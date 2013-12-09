ec0bd8db276250b0aab379f2c0844e47f147eb7d
Andre S <gymclothes@gmail.com>
2013-12-09 11:47:24 -0600

    softerrors update

--- Lib/Exception/ApiException.php
+++ Lib/Exception/ApiException.php
@@ -9,16 +9,14 @@ class ApiException extends CakeException
     public function __construct($message = 'There was error in the API. Please try again later.', $code = 500)
     {
         if (Configure::read('ApiManager.softErrors')) {
-            if (
-                (
-                    isset($this->getTrace()[0]['function']) &&
-                    stristr($this->getTrace()[0]['function'], 'api')
-                ) ||
-                (
-                    isset($this->getTrace()[0]['class']) &&
-                    stristr($this->getTrace()[0]['class'], 'request')
-                )
-            ) {
+            
+            $fromApi = (
+                (isset($this->getTrace()[0]['function']) && stristr($this->getTrace()[0]['function'], 'api')) && 
+                (isset($this->getTrace()[1]['function']) && stristr($this->getTrace()[1]['function'], 'api'))
+            );
+            $fromRequest = isset($this->getTrace()[0]['class']) && stristr($this->getTrace()[0]['class'], 'request');
+            
+            if ($fromApi || $fromRequest) {
                 echo json_encode([
                     'status' => 'error',
                     'methodName' => 'API',
