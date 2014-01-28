6f0e564b1572d7331d824ece06d1caff9d7e38c3
Ivan <mill-freedom@yandex.ru>
2014-01-28 18:40:10 +0200

    Update ApiMissingParamException.php
    fixes string as params internally

--- Lib/Exception/ApiMissingParamException.php
+++ Lib/Exception/ApiMissingParamException.php
@@ -8,17 +8,22 @@ class ApiMissingParamException extends ApiException
 {
     public function __construct($params = null, $code = 200)
     {
-        $message = [];
+        $message = '';
 
-        foreach ($params as $param) {
-            $message[] = $param . ' is missing.';
+        if (!empty($params)) {
+
+            if (!is_array($params))
+                $params = [$params];
+
+            foreach ($params as $param) {
+                $message .= $param . ' is missing. ';
+            }
         }
 
-        if (empty($message)) {
-            $message = ['Please check all required fields.'];
-        }
-        
-        parent::__construct(implode(' ', $message), $code);
+        if (empty($message))
+            $message = 'Please check all required fields.';
+
+        parent::__construct(trim($message), $code);
     }
 }
 
